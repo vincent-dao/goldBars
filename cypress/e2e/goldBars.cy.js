e7a04ea9e8482be5b1b2e368500d50d491675d5b
Vincent Dao <vincent.dao@joinlane.com>
2024-08-21 21:08:24 -0400

    chain with then() to yield prior variable

--- cypress/e2e/goldBars.cy.js
+++ cypress/e2e/goldBars.cy.js
@@ -1,15 +1,13 @@
 export const helperUtils = require('../fixtures/helpers.js');
 
 describe('Gold Bars Weighing', () => {
-  let goldBars = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
+  let goldBars = [0,1,2,3,4,5,6,7,8]
   let fakeBar = 0
 
   before(() => {
     cy.visit('https://sdetchallenge.fetch.com/');
     cy.contains('Weighings').should('be.visible');
   });
-  
   
   it('Find the fake bar', () => {    
     // This will test [0, 1, 2] against [3, 4, 5] and determine 
@@ -29,59 +27,53 @@ describe('Gold Bars Weighing', () => {
     helperUtils.getId('reset').eq(0).within(($li) => {
       cy.wrap($li).invoke('text').then((text) => {
         if (text === '=') {
-          cy.log('A')
-          cy.log(goldBars);
           goldBars = goldBars.slice(6);
         }
         else if (text === '<') {
-          cy.log('B')
-          cy.log(goldBars);
           goldBars = goldBars.slice(0,3);
         }
         else if (text === '>') {
-          cy.log('C')
-          cy.log(goldBars);
           goldBars = goldBars.slice(3,6);
         }
       })
-    })
+    }).then((bars) => {
+      // Reset the weighing scale
+      helperUtils.getId('reset').contains('Reset').click();
 
-    // Reset the weighing scale
-    helperUtils.getId('reset').contains('Reset').click();
+      // Now we will weigh 2 bars from the lightest group against each other
+      cy.log(goldBars);
+      helperUtils.getId('left_0').type(goldBars[0]);
+      helperUtils.getId('right_0').type(goldBars[1])
 
-    // Now we will weigh 2 bars from the lightest group against each other
-    helperUtils.getId('left_0').type(goldBars[0]);
-    helperUtils.getId('right_0').type(goldBars[1])
+      helperUtils.getId('weigh').click();
+      cy.contains('?').should('not.exist');
 
-    helperUtils.getId('weigh').click();
-    cy.contains('?').should('not.exist');
+      // Finding the bar which is the lightest
+      helperUtils.getId('reset').eq(0).within(($li) => {
+        cy.wrap($li).invoke('text').then((text) => {
+         if (text === '=') {
+            fakeBar = goldBars[2]
+          }
+          else if (text === '<') {
+            fakeBar = goldBars[0]
+          }
+          else if (text === '>') {
+            fakeBar = goldBars[1]
+          }
+        })
+      })
 
-    // Finding the bar which is the lightest
-    helperUtils.getId('reset').eq(0).within(($li) => {
-      cy.wrap($li).invoke('text').then((text) => {
-        if (text === '=') {
-          cy.log('D')
-          fakeBar = goldBars[2]
-        }
-        else if (text === '<') {
-          cy.log('E')
-          fakeBar = goldBars[0]
-        }
-        else if (text === '>') {
-          cy.log('F')
-          fakeBar = goldBars[1]
-        }
+      // Click on the coin that is the lightest 
+      cy.get('.coins').within(() => {
+        cy.contains(fakeBar).click();
+      })
+
+      // Receive the correct confirmation message!
+      cy.on('window:alert', (foundMessage) => {
+        expect(foundMessage).to.equal('Yay! You find it!')
       })
     })
-    console.log(goldBars)
 
-    helperUtils.getId('reset').contains('Reset').click();
-    cy.get('.coins').within(() => {
-      cy.contains(fakeBar).click();
-    })
-
-    cy.on('window:alert', (foundMessage) => {
-      expect(foundMessage).to.equal('Yay! You find it!')
-    })
+    
   })
 })
